9354d718cf79e8bb0b172cb1461585754813806b
cyongrui <chenyr06@hotmail.com>
2015-10-06 22:44:58 +0800

    delete and update method

--- src/main.java
+++ src/main.java
@@ -1,20 +1,28 @@
 import java.io.IOException;
+import java.util.*;
 
 public class main {
 	
 	
 	public static void main(String[] args){
 		Storage str = new Storage();
-		Task task = new Task("appendix", "13-3", "12:31", 0);
-		Task task2 = new Task("fishing", "4-9", "21:03", 1);
-		System.out.println(str.getFileName());
+		Date date0 = new Date(2015, 10, 6, 13,27);
+		Task task = new Task("appendix", date0, 0);
+		Task task1 = new Task("fishing", date0, 1);
+		Task task2 = new Task("Do CS homework", date0, 1);
+		Task task3 = new Task("Swimming", date0, 2);
 		
-		str.write(task);
-		str.write(task2);
-		str.undo();
-		str.redo();
+	//	str.write(task);
+	//	str.write(task2);
+		str.update(task3, 1);
+	//	str.delete(0);
+	//	str.undo();
+	//	str.redo();
 		try{
-//		str.read();
+/*		ArrayList<Task> arrList = str.read();
+		for(int i=0; i<arrList.size(); i++){
+			System.out.println(arrList.get(i));
+		}*/
 		System.out.println(Storage.getFileString("data.json"));
 		}
 		
