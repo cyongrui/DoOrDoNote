dd8b98c5f6ba6cc03f91cdaa3bafb8af33343a91
Yun Peng <chongyunpeng@gmail.com>
2015-10-18 19:20:42 +0800

    Added basic code for DeleteHandler. Does not handle exception cases yet

--- src/doordonote/commandfactory/DeleteHandler.java
+++ src/doordonote/commandfactory/DeleteHandler.java
@@ -1,10 +1,29 @@
 package doordonote.commandfactory;
 
+import doordonote.command.Command;
+import doordonote.command.DeleteCommand;
+
 public class DeleteHandler extends AbstractCommandHandler {
 
 	public DeleteHandler(String commmandBody) {
 		super(commmandBody);
-		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Command generateCommand() {
+		try {
+			int indexToDelete = Integer.parseInt(commandBody.trim());
+			if (indexToDelete <= 0) {
+				// throw exception
+				return null;
+			} else {
+				return new DeleteCommand(indexToDelete);
+			}
+		} catch (NumberFormatException e) {
+			// commandBody does is not a valid integer value
+			// throw exception
+			return null;
+		}		
 	}
 
 }
